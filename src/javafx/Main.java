70d82a3a17a0bcc6eb2dedb7dce51dfcacca6c0c
billytrinh <quanghoa.trinh@gmail.com>
2022-10-20 11:47:55 +0700

    java fx demo

--- src/javafx/Main.java
+++ src/javafx/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("demo.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("contact.fxml"));
         primaryStage.setTitle("T2204M JavaFX");
         primaryStage.setScene(new Scene(root,800,600));
         primaryStage.show();
