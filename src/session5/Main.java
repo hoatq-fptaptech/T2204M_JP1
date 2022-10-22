44d490503420be159b50701bffcde0371ab3037c
billytrinh <quanghoa.trinh@gmail.com>
2022-10-22 10:36:32 +0700

    java fx demo

--- src/session5/Main.java
+++ src/session5/Main.java
@@ -5,12 +5,15 @@ public class Main {
        try {
            int a = 10;
            int b = 0;
+           if(a > 5){
+               throw new Exception("Số to quá không chia được");
+           }
            int c = a / b;
            System.out.println("c = " + c);
        }catch (ArithmeticException e){
-           System.out.println("Arithmetic errors");
+           System.out.println(e.getMessage());
        }catch (Exception e){
-           System.out.println("Errors");
+           System.out.println(e.getMessage());
        }finally {
            // dù có catch hay ko thì vẫn chạy qua đây
        }
