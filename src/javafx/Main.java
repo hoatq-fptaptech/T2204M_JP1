024b1500d243f2ae70f94728ac114135a5a2bc3c
billytrinh <quanghoa.trinh@gmail.com>
2022-10-25 10:31:53 +0700

    java fx asm

--- src/javafx/Main.java
+++ src/javafx/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    public static Stage rootStage;
 
     public static void main(String[] args) {
         launch(args);
@@ -14,6 +15,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        rootStage = primaryStage;
         Parent root = FXMLLoader.load(getClass().getResource("students.fxml"));
         primaryStage.setTitle("T2204M JavaFX");
         primaryStage.setScene(new Scene(root,800,600));
--- src/javafx/StudentController.java
+++ src/javafx/StudentController.java
@@ -3,9 +3,13 @@ package javafx;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.util.Collections;
 import java.util.Comparator;
@@ -26,6 +30,8 @@ public class StudentController {
             String name = txtName.getText();
             String email = txtEmail.getText();
             Integer mark = Integer.parseInt(txtMark.getText());
+            if(mark < 0 || mark > 10)
+                throw new Exception("Vui lòng nhập điểm trong khoảng 0 -> 10");
             ls.add(new Student(name,email,mark));
             lvStudents.setItems(ls);
         }catch (Exception e){
@@ -46,5 +52,12 @@ public class StudentController {
         });
         sortName = !sortName;
         lvStudents.refresh();
+    }
+
+    public void goToContact(ActionEvent event) throws Exception{
+        Parent contactPage = FXMLLoader.load(getClass().getResource("contact.fxml"));
+        Scene sc = new Scene(contactPage,800,600);
+        Main.rootStage.setTitle("Contact JavaFX");
+        Main.rootStage.setScene(sc);
     }
 }
