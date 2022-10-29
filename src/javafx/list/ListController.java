363ff9635770862d38d279152e049bda2ff8f153
billytrinh <quanghoa.trinh@gmail.com>
2022-10-29 11:00:27 +0700

    table view demo - search

--- src/javafx/list/ListController.java
+++ src/javafx/list/ListController.java
@@ -15,7 +15,10 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class ListController implements Initializable {
     public static ObservableList<Student> ls = FXCollections.observableArrayList();
@@ -26,6 +29,7 @@ public class ListController implements Initializable {
     public TableColumn<Student,Integer> cMark;
     public TableColumn<Student,String> cGender;
     public TableColumn<Student, Button> cAction;
+    public TextField txtSearch;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -38,6 +42,7 @@ public class ListController implements Initializable {
         if(ls.size() == 0){
             ls.add(new Student("Nguyễn Hoàng Long","longnh@gmail.com",2,"Nam"));
             ls.add(new Student("Phan Hoàng Anh","anhph@gmail.com",4,"Nam"));
+            ls.add(new Student("Vũ Thế Anh","anhvt@gmail.com",4,"Nam"));
         }
         tbStudents.setItems(ls);
     }
@@ -48,15 +53,18 @@ public class ListController implements Initializable {
         Main.rootStage.setScene(sc);
     }
 
-    public void edit(){
+    public void search(ActionEvent actionEvent) {
         try {
-            if(tbStudents.getSelectionModel().getSelectedItem() == null){
-                throw new Exception("Vui lòng chọn sinh viên muốn sửa!");
+            String s = txtSearch.getText();
+            if(s.isEmpty()){
+                tbStudents.setItems(ls);
+                throw new Exception("Vui lòng nhập từ cần tìm kiếm");
             }
-            EditController.editedStudent = tbStudents.getSelectionModel().getSelectedItem();
-            Parent editForm = FXMLLoader.load(getClass().getResource("../edit/edit.fxml"));
-            Scene sc = new Scene(editForm,800,600);
-            Main.rootStage.setScene(sc);
+
+            ObservableList<Student> results = ls.stream()
+                    .filter(student -> student.getName().toLowerCase().contains(s.toLowerCase()))
+                    .collect(Collectors.toCollection(FXCollections::observableArrayList));
+            tbStudents.setItems(results);
         }catch (Exception e){
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Error!");
